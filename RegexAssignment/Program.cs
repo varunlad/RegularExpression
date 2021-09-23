2a56602122b87401cbe3d80a1e33006d6ff55b6f
varun <varunlad5@gmail.com>
2021-09-23 19:57:50 +0530

    [varun] UC1 validate User First Name

---
+++ RegexAssignment/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegexAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Regex058assignment name = new Regex058assignment();
+            name.Validation();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ RegexAssignment/Regex.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace RegexAssignment
+{
+    class Regex058assignment
+    {
+            string pattern = "^[A-Z]{1}[a-z]{3,}$";    
+            //writing the valid pattern
+            string[] inputs = { "varu","varUnl?ad","Varun134","SaksHi.com","DHVSJK", "ab", "abc@1.com","Varun" };//Validating some Example
+
+
+        /// <summary>
+        /// Validation this instance.
+        /// </summary>
+            public void Validation()
+            {
+                Regex regrex1 = new Regex(pattern);
+                Console.WriteLine("Validating User First name: ");
+                ItarateLoop(inputs, regrex1);
+            }          
+            public void ItarateLoop(string[] arr, Regex regrex1)
+            {
+                for (int i = 0; i < arr.Length; i++)//validation Loop
+                {
+                    bool result = regrex1.IsMatch(arr[i]);
+                    if (result)
+                    {
+                        Console.WriteLine(arr[i] + " It is --->" + "Valid");
+                    }
+                    else
+                    {
+                        Console.WriteLine(arr[i] + " It is --->" + "Invalid");
+                    }
+                }
+            }
+
+
+        
+    }
+}
